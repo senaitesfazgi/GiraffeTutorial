7e77a09f4eede36106a42d8731997bb624dd1ab0
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-08-21 09:15:35 -0600

    initial commit

---
+++ GiraffeTutorial/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiraffeTutorial
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+        
+    }
+}
---
+++ GiraffeTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiraffeTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
