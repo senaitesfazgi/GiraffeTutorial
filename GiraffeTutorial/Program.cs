419ee84abe8c9a6e8f541bfb60d8b89f4e057e6d
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-08-21 17:37:14 -0600

    Added constructors and static attributes

--- GiraffeTutorial/Program.cs
+++ GiraffeTutorial/Program.cs
@@ -6,12 +6,24 @@ namespace GiraffeTutorial
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("Living Courageously", "Joyce Meyer", 250);
+            Book book1 = new Book("Living Courageously", "Joyce Meyer", 250);  
+            Book book2 = new Book("Mere Christianity", "C.S Lewis", 200);
+            Book book3 = new Book("The Reason for my Hope ", "Billy Graham", 200);
 
-            Book book2 = new Book("Mere Christianity", "C>S Lewis", 200);
+            book2.title = "The Risen";
 
             Console.WriteLine(book1.title);
             Console.WriteLine(book2.title);
+
+            Song song1 = new Song("You never lost a battle", "Tauran Wells", 5);
+            Console.WriteLine(Song.songCount);
+            Song song2 = new Song("So will I", "HILL Song", 5);
+            Console.WriteLine(song2.getSongCount());
+
+           // Console.WriteLine(song1.title);  
+           // Console.WriteLine(song1.artist);
+             
+            
 
             Console.ReadLine();
 
---
+++ GiraffeTutorial/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiraffeTutorial
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount;
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+
+
+        }
+        public int getSongCount()
+        {
+            return songCount++;
+        }
+
+
+    }
+
+}
